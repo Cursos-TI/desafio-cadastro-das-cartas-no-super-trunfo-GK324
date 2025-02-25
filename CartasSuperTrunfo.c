750659252707edfc3b5e2c4eaf06c71a8363bbdf
GK <33989233+GK324@users.noreply.github.com>
2025-02-25 23:32:14 +0000

    Info da primeira carta  -- mermao q quebra cabeça...

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 
-int main {
+int main() {
     int pontosTuristicos;
-    int populaçao;
+    int populacao;
     char estado;
-    char cod[5];
-    char nomeDAcidade[10];
+    char codigo[5];
+    char nomeDAcidade[20];
     float area;
     float pib;
-// *Informação da primeira Carta "Recanto Das Emas"
+
+    int pontosTuristicos2;
+    int populacao2;
+    char estado2;
+    char codigo2[5];
+    char nomeDAcidade2[20];
+    float area2;
+    float pib2;
+// *Informação da Primeira e Segunda Carta "Celandia" e "Samambaia"
+
+    printf("Desafio SUPER TRUNFO: \n");
+    printf("Escolha um ESTADO: \n");
+    scanf("%s", &estado);
+    printf("Coloque um Codigo de carta ex A01: ");
+    scanf("%s", &codigo);
+    printf("Coloque uma Cidade: ");
+    scanf("%s", nomeDAcidade);
+    printf("Qual a População de sua Cidade? ");
+    scanf("%d", &populacao);
+    printf("Qual a Área em Km2 da sua Cidade? ");
+    scanf("%f", &area);
+    printf("Coloque o PIB: ");
+    scanf("%f", &pib );
+    printf("Números de Pontos Turistícos: ");
+    scanf("%d", &pontosTuristicos );
+// Informações da Primeira Carta
 
 
 
-
-
-
-
+return 0;
 
 }
